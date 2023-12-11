6a576961b05b0a5608e27bd48f1b35255d1c9f79
Christian Hohenbild <mail@christianhohenbild.de>
2023-12-11 20:43:09 +0100

    add example 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,66 @@
-import { useState } from 'react'
-import Logo from '/face-blowing-a-kiss.svg'
-import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
-import Experience from "./Experience";
-import './index.css'
+import { useTexture } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef, Suspense } from "react";
+import * as THREE from "three";
+import { v4 as uuidv4 } from "uuid";
+import './index.css';
 
-export default function App() {
-  
-  const [count, setCount] = useState(0)
+import vertexShader from './shader/vertexShader';
+import fragmentShader from './shader/fragmentShader'; 
 
- return (
+const DPR = 1;
+// Noise Texture
+// const NOISE_TEXTURE_URL = "https://cdn.maximeheckel.com/noises/noise2.png";
 
-  
-    <Canvas shadows camera={{ position: [0, 0, 2], fov: 40 }}>
-        <color 
-          attach="background" 
-          args={["#aaefef"]} />
-      <Experience />
-    </Canvas>
-  
+const Raymarching = () => {
+  const mesh = useRef();
+  const { viewport } = useThree();
+
+  const noisetexture = useTexture('./textures/noise2.png');
+  noisetexture.wrapS = THREE.RepeatWrapping;
+  noisetexture.wrapT = THREE.RepeatWrapping;
+
+  noisetexture.minFilter = THREE.NearestMipmapLinearFilter;
+  noisetexture.magFilter = THREE.NearestMipmapLinearFilter;
+
+  const uniforms = {
+    uTime: new THREE.Uniform(0.0),
+    uResolution: new THREE.Uniform(new THREE.Vector2()),
+    uNoise: new THREE.Uniform(null),
+  };
+
+  useFrame((state) => {
+    const { clock } = state;
+    mesh.current.material.uniforms.uTime.value = clock.getElapsedTime();
+    mesh.current.material.uniforms.uResolution.value = new THREE.Vector2(
+      window.innerWidth * DPR,
+      window.innerHeight * DPR
+    );
+    mesh.current.material.uniforms.uNoise.value = noisetexture;
+  });
+
+  return (
+    <mesh ref={mesh} scale={[viewport.width, viewport.height, 1]}>
+      <planeGeometry args={[1, 1]} />
+      <shaderMaterial
+        key={uuidv4()}
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+      />
+    </mesh>
   );
-}
+};
 
+const Scene = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 6] }} dpr={DPR}>
+      <Suspense fallback={null}>
+        <Raymarching />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+
+export default Scene;
